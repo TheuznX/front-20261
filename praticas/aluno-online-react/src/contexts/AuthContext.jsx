feedca4d7c970ffe601aedfd6a7909c0bf3d1ba8
unknown <2424290046@ACADEMICO.IESB.BR>
2026-06-15 10:02:32 -0300

    fix: ignora regra de fast refresh no context

--- praticas/aluno-online-react/src/contexts/AuthContext.jsx
+++ praticas/aluno-online-react/src/contexts/AuthContext.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import React, { createContext, useState, useContext } from 'react';
 import { login as apiLogin, logout as apiLogout } from '../services/authService';
 
