45848783c2b959ad11050209277be521ac64a5fc
TheuznX <matheus.gomes122004@gmail.com>
2026-06-15 12:47:14 -0300

    fix: força desativação da regra apenas-componentes no fim do arquivo

--- Downloads/front-20261-develop/praticas/aluno-online-react/eslint.config.js
+++ Downloads/front-20261-develop/praticas/aluno-online-react/eslint.config.js
@@ -24,6 +24,12 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+ 
+  {
+    files: ['**/*.{js,jsx}'],
+    rules: {
       'react-refresh/only-export-components': 'off',
     },
   },
